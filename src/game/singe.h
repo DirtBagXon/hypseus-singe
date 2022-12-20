7a63fee71d7c01ac1a90f89d1494c0374d401a10
DirtBagXon <noop@member.fsf.org>
2022-12-20 01:10:00 +0000

    Fix 8bit overlay arg placement

--- src/game/singe.h
+++ src/game/singe.h
@@ -72,7 +72,7 @@ class singe : public game
     bool fullsize_overlay = false;
     bool muteinit = false;
     bool notarget = false;
-    bool ocd = false;
+    bool ocv = false;
     bool oc = false;
 
     // g_ldp function wrappers (to make function pointers out of them)
