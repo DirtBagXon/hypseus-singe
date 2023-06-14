DirtBagXon/hypseus-singe

commit 7a50db57dac333c9f8d38166bddc01ca72d1c7b2

Enforce boundary check

src/game/singe/luretro.c
@@ -27,7 +27,8 @@
 
 enum {
     PATH_DAPHNE,
-    PATH_FRAMEWORK
+    PATH_FRAMEWORK,
+    PATH_END
 };
 
 static unsigned char g_retropath = 0;
@@ -39,42 +40,46 @@ void lua_set_retropath(unsigned char value) { g_retropath = value; }
 unsigned char inPath(const char* src, char* path)
 {
     char *s = strstr(src, path);
-
-    if (s != NULL)
-        return 1;
-    else
-        return 0;
+    if (s != NULL) return 1;
+    return 0;
 }
 
 void lua_retropath(const char *src, char *dst, int len)
 {
-    unsigned char r = 0, path = PATH_DAPHNE;
+    unsigned char r = 0, fin = 0, path = PATH_DAPHNE;
 
     if (inPath(src, "Framework")) path = PATH_FRAMEWORK;
 
-    for (int i = 0; i < len; src++, i++) {
-        if (i == 6) {
-            memcpy(dst, "/../", 4);
-            dst += 4;
-        }
-        if (*src == '/' && r < 0xf) {
-            r++;
-            continue;
-        }
-        if (r == 2) {
-            switch(path) {
-            case (PATH_FRAMEWORK):
-                memcpy(dst, "/", 1);
-                dst += 1;
-                break;
-            default:
-                memcpy(dst, ".daphne/", 8);
-                dst += 8;
-                break;
+    for (int i = 0; i < (len - 2); src++, i++) {
+        if (!fin) {
+            if (*src == '\0') {
+                fin = PATH_END;
+                continue;
             }
-            r = 0xf; //bool
+            if (i == 6) {
+                memcpy(dst, "/../", 4);
+                dst += 4;
+            }
+            if (*src == '/' && r < 0xf) {
+                r++;
+                continue;
+            }
+            if (r == 2) {
+                switch(path) {
+                case (PATH_FRAMEWORK):
+                    memcpy(dst, "/", 1);
+                    dst += 1;
+                    break;
+                default:
+                    memcpy(dst, ".daphne/", 8);
+                    dst += 8;
+                    break;
+                }
+                r = 0xf; //bool
+            }
+            *dst = *src;
+            dst++;
         }
-        *dst = *src;
-        dst++;
     }
+    *dst = '\0';
 }

src/game/game.cpp
@@ -353,6 +353,7 @@ bool game::init_video()
 
     if (m_area < area || m_overlay_depth == GAME_OVERLAY_DEPTH) {
         video::init_display();
+        m_area = area;
     }
 
     // if this particular game uses video overlay (most do)
@@ -405,7 +406,6 @@ bool game::init_video()
 
     // Log some stats
     video::notify_stats(m_video_overlay_width, m_video_overlay_height);
-    if (area > m_area) m_area = area;
 
     return (result);
 }

commit cd069d439b97ccdf4a5f070f41c5558fd8420c96

Fix lfs_dir on older games broken in 7a50db57dac333c9f8d38166bddc01ca72d1c7b2

src/game/singe/luretro.c
@@ -54,7 +54,6 @@ void lua_retropath(const char *src, char *dst, int len)
         if (!fin) {
             if (*src == '\0') {
                 fin = PATH_END;
-                continue;
             }
             if (i == 6) {
                 memcpy(dst, "/../", 4);